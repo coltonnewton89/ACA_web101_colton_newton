e12af5250072abbafc27401642c125fd14e44974
coltonnewton89 <52718393+coltonnewton89@users.noreply.github.com>
2019-09-05 18:57:12 -0500

    fc complete

---
+++ src/crash/hi.js
@@ -0,0 +1,3 @@
+function player() {
+  console.log("almost");
+}
---
+++ src/finalcheck/fc.js
@@ -0,0 +1,47 @@
+//creating new function titled whatever I want
+function play() {
+  //inside this function, we'll create a variable that pulls a specific sound
+  var audio = document.getElementById("hato");
+  //once a specific sound is attached to the var, it'll play!
+  audio.play();
+}
+
+function play1() {
+  var audio = document.getElementById("hatc");
+  audio.play();
+}
+
+function play2() {
+  var audio = document.getElementById("snare");
+  audio.play();
+}
+
+function play3() {
+  var audio = document.getElementById("crash");
+  audio.play();
+}
+
+function play4() {
+  var audio = document.getElementById("hit");
+  audio.play();
+}
+
+function play5() {
+  var audio = document.getElementById("midt");
+  audio.play();
+}
+
+function play6() {
+  var audio = document.getElementById("ride");
+  audio.play();
+}
+
+function play7() {
+  var audio = document.getElementById("floort");
+  audio.play();
+}
+
+function play8() {
+  var audio = document.getElementById("bas");
+  audio.play();
+}
